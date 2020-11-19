b0e6c5b9f3618282e1efbda87203f9a630ccb38d
Avery Little <avery.little@industrystar.com>
2020-11-19 02:48:39 -0500

    stayed up to do a few exercises

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,66 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>
-      {props.course.name}
-    </h1>
-  )
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick} feedback={text}>
+    {text}
+  </button>
+)
+
+const calcAvg = (good, neutral, bad) => {
+  return ((good + neutral) - bad)/3
 }
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercise}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-    <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises}/>
-    <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises}/>
-    <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
-    </p>
-  )
+const calcPositive = (good, neutral, bad) => {
+  return (good/(good + neutral + bad)) * 100
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  // These need to use useState hooks
+  let average = 0;
+  let positive = 0;
+
+  const handleFeedback = (feedback) => {
+    if(feedback === 'good') {
+      setGood(good + 1);
+    }
+    else if(feedback === 'neutral') {
+      setNeutral(neutral + 1);
+    }
+    else {
+      setBad(bad + 1);
+    }
+    
+    average = calcAvg(good, neutral, bad);
+    positive = calcPositive(good, neutral, bad);
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h2>give feedback</h2>
+      <div>
+        <Button onClick={() => {handleFeedback('good')}} text='good'/>
+        <Button onClick={() => {handleFeedback('neutral')}} text='neutral'/>
+        <Button onClick={() => {handleFeedback('bad')}} text='bad'/>
+      </div>
+      <h2>statistics</h2>
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + bad + neutral}</p>
+        <p>average {average}</p>
+        <p>positive {positive}%</p>
+      </div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
