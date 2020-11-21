428bbb3c203a1aabbe47247602a2a67d0ca23899
Avery Little <avery.little@industrystar.com>
2020-11-20 23:15:42 -0500

    finished part 1 exercises

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,12 +1,55 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick} feedback={text}>
+    {text}
+  </button>
+)
+
 const App = (props) => {
+
+  const initializeVotes = () => {
+    return new Array(anecdotes.length).fill(0)
+  }
+  
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(initializeVotes())
+  // The top anecdote is just set to the first one
+  const [topAnecdote, setTopAnecdote] = useState(0)
+  
+
+  const selectAnecdote = () => {
+    const max = anecdotes.length
+    let index = Math.floor(Math.random() * max)
+    setSelected(index)
+  }
+
+  const vote = () => {
+    let copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+
+    // Compare the current anecdote to the current top anecdote
+    // in the updated copy of the votes array
+    if(copy[selected] > copy[topAnecdote]) 
+      setTopAnecdote(selected);
+  }
 
   return (
     <div>
+      <h2>Current Anecdote</h2>
       {props.anecdotes[selected]}
+      <br></br>
+      has {votes[selected]} votes
+      <div>
+        <Button onClick={selectAnecdote} text={'next anecdote'}/>
+        <Button onClick={vote} text={'vote'}/>
+      </div>
+      <h2>Top Anecdote</h2>
+      {anecdotes[topAnecdote]}
+      <br></br>
+      has {votes[topAnecdote]} votes
     </div>
   )
 }
