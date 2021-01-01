01d774207b0d7db0bd5b7dd90b2951c87af1c5f0
Avery Little <avery.little@industrystar.com>
2021-01-01 14:20:00 -0500

    part5 A exercises done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -26,7 +26,6 @@ blogsRouter.post('/', async (request, response, next) => {
           title: body.title,
           author: body.author,
           url: body.url,
-          likes: body.likes,
           user: user
         })
 
